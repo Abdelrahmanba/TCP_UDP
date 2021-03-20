ed199b75d70433df97f78b51100c5b52101a1be2
Abdelrahman <babaa.abd@gmail.com>
2021-03-20 13:48:53 +0200

    first commit

--- src/TCP/TCP_Client.java
+++ src/TCP/TCP_Client.java
@@ -1,4 +1,28 @@
 package TCP;
 
-public class git TCP_Client {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class TCP_Client {
+    public static void main(String[] argv){
+        int portNo = 6789;
+        String serverName = "192.168.1.101";
+        Socket clientSocket;
+        BufferedReader stdUserReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            clientSocket = new Socket(serverName,portNo);
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            outToServer.writeBytes(stdUserReader.readLine()+"\n");
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.printf("ERROR:Couldn't creat user socket, probably  port:%d is currently in use%n",portNo);
+        }
+
+
+
+    }
 }
--- src/TCP/TCP_Server.java
+++ src/TCP/TCP_Server.java
@@ -1,4 +1,35 @@
 package TCP;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static java.lang.System.exit;
+
 public class TCP_Server {
+    public static void main(String[] argv) {
+        ServerSocket listeningSocket = null;
+        int portNo = 6789;
+        try {
+            listeningSocket = new ServerSocket(6789);
+        } catch (IOException e) {
+            System.err.printf("ERROR:Couldn't creat socket, probably port:%d is currently in use.", portNo);
+            exit(1);
+        }
+
+        while (true) {
+            try{
+                Socket connectionSocket = listeningSocket.accept();
+                BufferedReader inFormUser = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                System.out.println(inFormUser.readLine());
+            }
+            catch (Exception e){
+
+            }
+        }
+
+
+    }
 }
